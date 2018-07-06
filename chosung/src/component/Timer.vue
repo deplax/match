84d526245678a0d85625259c1687d32b56fdf923
deplax <sturdyegg@gmail.com>
2018-07-06 19:51:12 +0900

    [feat]타이머 추가

--- chosung/src/component/Timer.vue
+++ chosung/src/component/Timer.vue
@@ -12,13 +12,13 @@
       <md-card-content>
         <div class="md-layout md-gutter">
           <div class="md-layout-item md-layout md-gutter md-alignment-center">
-            <div class="md-layout-item md-size-50">
+            <div class="md-layout-item md-size-60">
               <md-content class="md-elevation-1">
-                <span class="timer center">00:00</span>
+                <span class="timer center">00.000</span>
               </md-content>
               <md-field>
                 <label>Number</label>
-                <md-input type="number"></md-input>
+                <md-input type="number" v-model.number="count"></md-input>
               </md-field>
             </div>
           </div>
@@ -39,7 +39,7 @@
           <div class="md-layout-item md-layout md-gutter md-alignment-center">
             <div class="md-layout-item md-size-95">
               <div>
-                <md-button class="md-raised md-primary center start">start</md-button>
+                <md-button class="md-raised md-primary center start" v-on:click="start">start</md-button>
               </div>
             </div>
           </div>
@@ -51,7 +51,40 @@
 </template>
 
 <script>
-  export default {}
+  let timerMillisecond = 1000 * 10;
+  let timerTime = Date.now() + timerMillisecond;
+  let diff = timerTime - Date.now();
+  let displaySecond = (diff % (1000 * 60)) / 1000;
+  let displayMilisecond = diff % 1000;
+
+  export default {
+    data() {
+      return {
+        count: 10,
+        currentTime: 0,
+        timerStatus: "STOP"
+      }
+    },
+    methods: {
+      start() {
+        let timerMillisecond = 1000 * this.count;
+        let timerTime = Date.now() + timerMillisecond;
+
+        const timer = setInterval(function(){
+          let diff = timerTime - Date.now();
+          let displaySecond = (diff % (1000 * 60)) / 1000;
+          let displayMillisecond = diff % 1000;
+          console.log(displaySecond + ":" + displayMillisecond / 100)
+        }, 10);
+      },
+      stop() {
+
+      },
+      pause() {
+
+      }
+    }
+  }
 </script>
 
 <style scoped>
